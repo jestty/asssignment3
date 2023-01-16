b74848c6ed9f3aa860226750eb4e730dc15386a8
jestty <121748586+jestty@users.noreply.github.com>
2023-01-17 06:44:28 +0900

    Add files via upload

---
+++ javaScript/script.js
@@ -0,0 +1,45 @@
+'use strict';
+//man hinh to
+const active = function () {
+  document.querySelector('.content-kn').classList.add('display1');
+  document.querySelector('.edu-hv').classList.add('display3');
+  document.querySelector('.active-hd').classList.add('display1');
+  document.querySelector('.hobby-sth').classList.add('display4');
+  document.querySelector('.content-lang').classList.add('display3');
+  document.querySelector('.content-skills').classList.add('display1');
+};
+//man hinh mobile
+const active1 = function () {
+  document
+    .querySelector('.content-kn')
+    .classList.add('display1', 'index', 'radius');
+  document
+    .querySelector('.edu-hv')
+    .classList.add('display4', 'index', 'radius');
+  document
+    .querySelector('.active-hd')
+    .classList.add('display3', 'index', 'text-dark', 'radius');
+  document
+    .querySelector('.hobby-sth')
+    .classList.add('display4', 'index', 'radius');
+  document
+    .querySelector('.content-lang')
+    .classList.add('display2', 'index', 'radius');
+  document
+    .querySelector('.content-skills')
+    .classList.add('display1', 'index', 'radius');
+  document.querySelector('.active-hd').classList.remove('bg-primary');
+};
+//lay kick thuoc trinh duyet wed
+const getWidth = () => {
+  return Math.max(
+    document.querySelector('body').scrollWidth,
+    document.querySelector('body').offsetWidth
+  );
+};
+
+if (getWidth() > 576) {
+  active();
+} else {
+  active1();
+}
